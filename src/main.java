03ab0ed243199555441aa9b8169079a90e1b4962
nasqui <nasqui@espol.edu.ec>
2022-04-13 15:34:33 -0500

    method compareTime

--- src/main.java
+++ src/main.java
@@ -1,3 +1,4 @@
+import java.time.Duration;
 import java.time.LocalTime;
 import java.util.LinkedList;
 
@@ -14,27 +15,84 @@ public class main {
 		//recorrerSchedules(scheduleByEmployees);
 		
 		LinkedList<Employee> schedulesByEmployees = FileRead.leerArchivo();
-		for (int i = 0; i < schedulesByEmployees.size(); i++) {
-			Employee employee = schedulesByEmployees.get(i);
-			LinkedList<Schedule> schedulees  = employee.getSchedule();
-			for (int y = 0; y < schedulees.size(); y++) {		
-				Schedule schedule1 = schedulees.get(y);
-				Schedule schedule2 = schedulees.get(y+1);
-				compareSchedules(schedule1, schedule2);
+		compareSchedules(schedulesByEmployees,0);
+	}
+	
+	public static void compareSchedules(LinkedList<Employee> schedulesByEmployees, int i){
+		if(schedulesByEmployees.size()==2) {
+			
+			Employee employee1 = schedulesByEmployees.get(i);
+			Employee employee2 = schedulesByEmployees.get(i+1);
+			String tupla = employee1.getName() +  "-" +  employee2.getName();
+			//System.out.println(tupla);
+			
+			LinkedList<Schedule> horario1 = schedulesByEmployees.get(i).getSchedule();
+			LinkedList<Schedule> horario2 = schedulesByEmployees.get(i+1).getSchedule();
+			System.out.println("horario1 " + horario1);
+			System.out.println("horario2 " + horario2);
+			int contador =0;
+			for (int x = 0; x < horario1.size(); x++) {
+				Schedule h1 = horario1.get(x);
+				Schedule h2 = horario2.get(x);
+				contador = tiempo( h1,  h2,contador);
+			}
+			System.out.println(tupla+ "-" + contador);
+		}
+		
+	}
+	public static int tiempo(Schedule h1, Schedule h2, int contador) {
+		//System.out.println(h1.getDay());
+		//System.out.println(h2.getDay());
+		if(h1.getDay().equals(h2.getDay())) {
+			System.out.print(h1.getTimeStart());
+			System.out.println(h1.getTimeFinish());
+
+			System.out.print(h2.getTimeStart());
+			System.out.println(h2.getTimeFinish());
+			Duration duration = Duration.between(h2.getTimeStart(), h2.getTimeFinish());
+			//TimeStartA==TimeStartB and TimeFinishA==TimeFinishB
+			if (h1.getTimeStart().equals(h2.getTimeStart()) && h1.getTimeFinish().equals(h2.getTimeFinish())){
+				contador ++;
+				System.out.println("equals");
+			
+			}//TimeStartA<TimeStartB and TimeFinishA>TimeFinishB
+			else if(h1.getTimeStart().isBefore(h2.getTimeStart()) && h1.getTimeFinish().isAfter(h2.getTimeFinish())) {
+				contador ++;
+				System.out.println("A contiene a B");
+			}//TimeStartA<=TimeStartB and TimeFinishA<=TimeFinishB and TimeFinishA>TimeStartB
+			else if((h1.getTimeStart().isBefore(h2.getTimeStart() )|| h1.getTimeStart().equals(h2.getTimeStart()))&& (h1.getTimeFinish().isBefore(h2.getTimeFinish())||h1.getTimeFinish().equals(h2.getTimeFinish())) && h1.getTimeFinish().isAfter(h2.getTimeStart())) {
+				contador ++;
+				System.out.println("2");
+			}//TimeStartA>=TimeStartB and TimeFinishA>TimeFinishB and TimeFinishA<TimeStartB
+			else if((h1.getTimeStart().isAfter(h2.getTimeStart()) ||h1.getTimeStart().equals(h2.getTimeStart())) && h1.getTimeFinish().isAfter(h2.getTimeFinish()) && h1.getTimeStart().isBefore(h2.getTimeFinish())) {
+				contador ++;
+				System.out.println("3");
+			}//TimeStartB<TimeStartA and TimeFinishB>TimeFinishA
+			else if (h2.getTimeStart().isBefore(h1.getTimeStart()) && h2.getTimeFinish().isAfter(h1.getTimeFinish())){
+				contador ++;
+				System.out.println("B contiene a A");
 			}
 		}
+		
+		return contador;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	/*public static void iterateList(LinkedList<Employee> linkedList){	
-		
-		linkedList.forEach( (element) -> System.out.print(element + " "));
-		
-	}*/
-	public static void compareSchedules(Schedule schedule1, Schedule schedule2){
-		//Schedule schedule1 = new Schedule();
-		//Schedule schedule2 = new Schedule();
-		
-		
-	}
+	
+	linkedList.forEach( (element) -> System.out.print(element + " "));
+	
+}*/
+	
+	
+	
 	public static void recorrerSchedules(LinkedList<Employee> linkedList){
 		for (int i = 0; i < linkedList.size(); i++) {
 			LinkedList<Schedule> scheduleByEmployee = linkedList.get(i).getSchedule();
